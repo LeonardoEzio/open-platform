1679a5034c8099e24b9658a111f0eaefa4f550b1
LeonardoEzio <LeonardoEzio@163.com>
2024-03-28 00:11:10 +0800

    init

---
+++ business-gateway/src/main/java/leonardo/ezio/personal/BusinessGatewayApplication.java
@@ -0,0 +1,16 @@
+package leonardo.ezio.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BusinessGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessGatewayApplication.class);
+    }
+
+}
